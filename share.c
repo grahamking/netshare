fee2a622dd6ad2f8a7fdd761d804d5d21acea251
Graham King <graham@gkgk.org>
2012-10-06 14:14:56 -0700

    epoll out working

--- share.c
+++ share.c
@@ -20,29 +20,88 @@
 
 #include <sys/epoll.h>
 
-#define HEADERS "HTTP/1.0 200 OK\n\n"
+#define HEAD_TMPL "HTTP/1.0 200 OK\nContent-Type: text/plain\nContent-Length: %ld\n\n"
 
-// Accept and handle a single connection
-ssize_t handleSingle(int connfd, int datafd, int datasz) {
+off_t offset[100];
+char *headers;
 
+// Process an epoll event
+void handleEvent(
+        struct epoll_event *evp,
+        int sockfd,
+        int datafd,
+        int efd,
+        off_t datasz) {
+
+    int err;
+    int connfd = -1;
     ssize_t wrote;
 
-    write(connfd, HEADERS, strlen(HEADERS));
-    wrote = sendfile(connfd, datafd, NULL, datasz);
-    if (wrote == -1) {
-        error(EXIT_FAILURE, errno, "Error senfile");
-    }
+    printf("Is ready: %d\n", evp->data.fd);
+    if (evp->data.fd == sockfd) {
 
-    return wrote;
+        connfd = accept4(sockfd, NULL, 0, SOCK_NONBLOCK);
+        if (connfd == -1) {
+            error(EXIT_FAILURE, errno, "Error 'accept' on socket");
+        }
+
+        evp->events = EPOLLOUT;
+        evp->data.fd = connfd;
+        err = epoll_ctl(efd, EPOLL_CTL_ADD, connfd, evp);
+        if (err == -1) {
+            error(EXIT_FAILURE, errno, "Error adding to epoll descriptor");
+        }
+
+    } else {
+        connfd = evp->data.fd;
+
+        if (offset[connfd] == 0) {
+            // Later: Set TCP CORK
+            write(connfd, headers, strlen(headers));
+        }
+        wrote = sendfile(connfd, datafd, &offset[connfd], datasz - offset[connfd]);
+        if (wrote == -1) {
+            if (errno == EAGAIN) {
+                return;
+            } else {
+                error(EXIT_FAILURE, errno, "Error senfile");
+            }
+        }
+
+        printf("%d: Wrote %ld / %ld bytes\n",
+                connfd,
+                offset[connfd],
+                datasz);
+
+        if (offset[connfd] >= datasz) {
+
+            err = epoll_ctl(efd, EPOLL_CTL_DEL, connfd, NULL);
+            if (err == -1) {
+                error(EXIT_FAILURE, errno, "Error removing connfd from epoll");
+            }
+
+            printf("Closing %d\n", connfd);
+            shutdown(connfd, SHUT_WR);
+            /*
+            err = close(connfd);  // Also removes it from epoll
+            if (err == -1) {
+                error(EXIT_FAILURE, errno, "Error closing connfd");
+            }
+
+            offset[connfd] = 0;
+            */
+        }
+    }
 }
 
 // Start here
 int main(int argc, char **argv) {
 
-    ssize_t wrote;
     struct in_addr localhost;
     struct sockaddr_in addr;
     struct stat datastat;
+
+    memset(&offset, 0, 100);
 
     int sockfd = socket(AF_INET, SOCK_STREAM | SOCK_NONBLOCK, 0);
     if (sockfd == -1) {
@@ -80,6 +139,10 @@ int main(int argc, char **argv) {
         error(EXIT_FAILURE, errno, "Error opening payload");
     }
     fstat(datafd, &datastat);
+    off_t datasz = datastat.st_size;
+
+    headers = malloc(strlen(HEAD_TMPL) + 12);
+    sprintf(headers, HEAD_TMPL, datasz);
 
     int efd = epoll_create(1);
     if (efd == -1) {
@@ -94,51 +157,31 @@ int main(int argc, char **argv) {
         error(EXIT_FAILURE, errno, "Error adding to epoll descriptor");
     }
 
-    int hits = 0;
-    int connfd = -1;
-    while (1) {
-        if (lseek(datafd, 0, SEEK_SET) == -1) {
-            error(EXIT_FAILURE, errno, "Error seeking back to payload start");
-        }
+    int i;
+    int num_ready;
+    struct epoll_event events[100];
 
-        err = epoll_wait(efd, &ev, 1, -1);
-        if (err == -1) {
+    while (1) {
+
+        num_ready = epoll_wait(efd, events, 100, -1);
+        if (num_ready == -1) {
             error(EXIT_FAILURE, errno, "Error on epoll_wait");
         }
 
-        printf("Is ready: %d\n", ev.data.fd);
-        if (ev.data.fd == sockfd) {
-
-            connfd = accept4(sockfd, NULL, 0, SOCK_NONBLOCK);
-            if (connfd == -1) {
-                error(EXIT_FAILURE, errno, "Error 'accept' on socket");
-            }
-
-            ev.events = EPOLLIN;
-            ev.data.fd = connfd;
-            err = epoll_ctl(efd, EPOLL_CTL_ADD, connfd, &ev);
-            if (err == -1) {
-                error(EXIT_FAILURE, errno, "Error adding to epoll descriptor");
-            }
-
-        } else {
-            connfd = ev.data.fd;
-            wrote = handleSingle(connfd, datafd, datastat.st_size);
-
-            err = epoll_ctl(efd, EPOLL_CTL_DEL, connfd, NULL);
-            if (err == -1) {
-                error(EXIT_FAILURE, errno, "Error removing connfd from epoll");
-            }
-            close(connfd);
-
-            hits++;
-            printf("%d: Wrote %d / %d bytes\n",
-                    hits, (int)wrote, (unsigned int) datastat.st_size);
+        for (i = 0; i < num_ready; i++) {
+            handleEvent(&events[i], sockfd, datafd, efd, datasz);
         }
     }
 
-    close(datafd);
-    close(sockfd);
+    err = close(datafd);
+    if (err == -1) {
+        error(EXIT_FAILURE, errno, "Error closing payload fd");
+    }
+
+    err = close(sockfd);
+    if (err == -1) {
+        error(EXIT_FAILURE, errno, "Error closing socket fd");
+    }
 
     return 0;
 }
